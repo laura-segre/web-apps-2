ff1589bd39da017e3e163943063f918d3a3f64a0
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-02-18 20:44:56 +0000

    Made changes to dice and bitcoins

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,9 @@
 class TacosController < ApplicationController
 
   def index
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
+
+    html = ""
     # render :template => "tacos/index"
   end
 
