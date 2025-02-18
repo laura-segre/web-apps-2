3cdc2381a0228a84a70cba8ed2834cb7aeae5324
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-02-18 22:32:41 +0000

    many changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+    #get("companies/:new", :controller => "companies", :action => "new")
+  #get("companies/:id", :controller => "companies", :action => "show")
+  #post("/companies/create", :controller => "companies", :action => "create")
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
+    # get("/contacts", {:controller => "contacts", :action => "show"})
 
   resources "posts"
   # get("/posts", {:controller => "posts", :action => "index"})
